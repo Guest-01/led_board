4e8c7027e11822a9f76f67a4ceb78cbda6b209df
atHome <cloudonly21@gmail.com>
2023-12-09 23:23:04 +0900

    add fontSize, speed (marquee)

--- lib/led_screen.dart
+++ lib/led_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:marquee/marquee.dart';
 
 class LedScreen extends StatefulWidget {
   final Color backgroundColor;
   final Color textColor;
   final String content;
+  final double fontSize;
+  final double speed;
 
   const LedScreen({
     super.key,
     required this.backgroundColor,
     required this.textColor,
     required this.content,
+    required this.fontSize,
+    required this.speed,
   });
 
   @override
@@ -40,13 +45,23 @@ class _LedScreenState extends State<LedScreen> {
     return Scaffold(
       backgroundColor: widget.backgroundColor,
       body: Center(
-        child: Text(
-          widget.content,
-          style: TextStyle(
-            color: widget.textColor,
-            fontSize: 148,
-          ),
-        ),
+        child: widget.speed == 0 || widget.content.isEmpty
+            ? Text(
+                widget.content,
+                style: TextStyle(
+                  color: widget.textColor,
+                  fontSize: widget.fontSize,
+                ),
+              )
+            : Marquee(
+                text: widget.content,
+                style: TextStyle(
+                  color: widget.textColor,
+                  fontSize: widget.fontSize,
+                ),
+                velocity: widget.speed,
+                blankSpace: 30,
+              ),
       ),
     );
   }
--- lib/make_screen.dart
+++ lib/make_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:led_board/led_screen.dart';
 import 'package:led_board/widgets/color_circle_row.dart';
+import 'package:marquee/marquee.dart';
 
 class MakeScreen extends StatefulWidget {
   const MakeScreen({
@@ -13,7 +14,20 @@ class MakeScreen extends StatefulWidget {
 
 class _MakeScreenState extends State<MakeScreen> {
   TextEditingController controller = TextEditingController();
+  List<(double size, String label)> fontSizeList = [
+    (86, '작게'),
+    (128, '보통'),
+    (172, '크게'),
+  ];
+  List<(double speed, String label)> speedList = [
+    (0, '정지'),
+    (100, '보통'),
+    (180, '빠름'),
+  ];
   List<Color> colorList = [Colors.black, Colors.white, Colors.amber];
+
+  late double fontSize;
+  late double speed;
   late Color textColor;
   late Color backgroundColor;
 
@@ -22,6 +36,8 @@ class _MakeScreenState extends State<MakeScreen> {
     // TODO: 저장된 마지막 값 불러오기
     super.initState();
     controller.text = "";
+    fontSize = 128;
+    speed = 0;
     textColor = Colors.white;
     backgroundColor = Colors.black;
   }
@@ -60,12 +76,30 @@ class _MakeScreenState extends State<MakeScreen> {
                     color: backgroundColor,
                   ),
                   child: Center(
-                    child: Text(
-                      controller.text,
-                      style: TextStyle(color: textColor, fontSize: 56),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
+                    child: speed == 0 ||
+                            controller.text
+                                .isEmpty // Marquee text should be not empty
+                        ? Text(
+                            controller.text,
+                            style: TextStyle(
+                              color: textColor,
+                              fontSize: fontSize / 2.2, // 실제 비율에 맞추기 위함
+                            ),
+                            overflow: TextOverflow.clip,
+                            maxLines: 1,
+                          )
+                        : Marquee(
+                            text: controller.text,
+                            style: TextStyle(
+                              color: textColor,
+                              fontSize: fontSize / 2.2,
+                            ),
+                            velocity: speed,
+                            blankSpace: 30,
+                            pauseAfterRound: Duration.zero,
+                            accelerationDuration: Duration.zero,
+                            decelerationDuration: Duration.zero,
+                          ),
                   ),
                 ),
                 const Text('미리보기'),
@@ -90,6 +124,70 @@ class _MakeScreenState extends State<MakeScreen> {
                   },
                 ),
                 const SizedBox(height: 10),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 6, 7, 6),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('글자크기'),
+                        SegmentedButton<double>(
+                          style: const ButtonStyle(
+                            visualDensity: VisualDensity.compact,
+                          ),
+                          segments: [
+                            for (var sizeAndLabel in fontSizeList)
+                              ButtonSegment(
+                                value: sizeAndLabel.$1,
+                                label: Text(sizeAndLabel.$2),
+                              ),
+                          ],
+                          selected: {fontSize},
+                          onSelectionChanged: (newSelections) {
+                            fontSize = newSelections.first;
+                            setState(() {});
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 6, 7, 6),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('글자속도'),
+                        SegmentedButton<double>(
+                          style: const ButtonStyle(
+                            visualDensity: VisualDensity.compact,
+                          ),
+                          segments: [
+                            for (var speedAndLabel in speedList)
+                              ButtonSegment(
+                                value: speedAndLabel.$1,
+                                label: Text(speedAndLabel.$2),
+                              ),
+                          ],
+                          selected: {speed},
+                          onSelectionChanged: (newSelections) {
+                            speed = newSelections.first;
+                            setState(() {});
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
                 Card(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
@@ -144,7 +242,7 @@ class _MakeScreenState extends State<MakeScreen> {
                 ),
                 const Padding(
                   padding: EdgeInsets.symmetric(vertical: 15),
-                  child: Text('TODO: 글꼴, 글자크기, 흐르는 속도, 마지막 값 기억'),
+                  child: Text('TODO: 글꼴, 마지막 값 기억'),
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
@@ -158,6 +256,8 @@ class _MakeScreenState extends State<MakeScreen> {
                           backgroundColor: backgroundColor,
                           textColor: textColor,
                           content: controller.text,
+                          fontSize: fontSize,
+                          speed: speed,
                         ),
                       ),
                     );
